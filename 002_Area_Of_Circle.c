3dd67dd70d2bce9ad0f93ac542b4a516f06df6f1
Bhavna Sahu <sahubhavna012@gmail.com>
2026-08-16 10:14:56 +0530

    add

--- 002_Area_Of_Circle.c
+++ 002_Area_Of_Circle.c
@@ -3,15 +3,12 @@
 void main()
 {
 	int r;
-	float pi, area, circum;
+	float pi, area;
 	printf("enter the value of radius: ");
 	scanf("%d", &r);
 
 	pi = 3.14;
 	area = pi * r * r;
 	printf("the area of circle is : %.2f\n", area);
-	circum = 2 * pi * r;
-	printf("the circumference of the circle is : %.2f", circum);
-	getch();
-	clrscr();
+
 }
